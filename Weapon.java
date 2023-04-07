f134645b587a2559c511fbc7efd216e7a647e3b2
ArturPletniuk <116193546+ArturPletniuk@users.noreply.github.com>
2023-04-07 08:54:55 +0300

    Delete Weapon.java

--- Weapon.java
+++
@@ -1,7 +0,0 @@
-public abstract class Weapon implements Weaponable {
-    protected int pointOfDamage;
-
-    public Weapon(int pointOfDamage) {
-        this.pointOfDamage = pointOfDamage;
-    }
-}
