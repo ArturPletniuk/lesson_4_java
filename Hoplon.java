b868d107316d2150c267663b8ec4891238c8bfab
Artur <Pletnyk95@mail.ru>
2023-04-07 08:25:41 +0300

    Hoplon

---
+++ Hoplon.java
@@ -0,0 +1,13 @@
+public class Hoplon extends Shield{
+
+    public Hoplon(int size, int armorReserve) {
+        super(size, armorReserve);
+    }
+
+    @Override
+    public String toString() {
+        return "Hoplon{" +
+                "armorReserve=" + armorReserve +
+                '}';
+    }
+}
